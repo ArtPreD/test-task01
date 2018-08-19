3936fa941df80ff7926ad2b04fd78601c91e6c02
Artem Predvoditelev <vladglad39@gmail.com>
2018-08-19 14:56:02 +0300

    close all database resource

--- src/main/java/database/service/CooperatorsService.java
+++ src/main/java/database/service/CooperatorsService.java
@@ -4,6 +4,7 @@ import database.JDBCPostgreSQL;
 import database.entity.Cooperator;
 import database.service.utils.ListCreator;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,49 +16,60 @@ public class CooperatorsService {
     private JDBCPostgreSQL jdbcPostgreSQL;
 
     public CooperatorsService() {
-        this.jdbcPostgreSQL = JDBCPostgreSQL.getInstance();
+        this.jdbcPostgreSQL = new JDBCPostgreSQL();
     }
 
     public List<Cooperator> findAllCoopFromDep(int departmentID) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("SELECT * FROM COOPERATORS WHERE DEPARTMENT_ID=?");
         statement.setObject(1, departmentID);
         List<Cooperator> cooperators = ListCreator.createCoopList(statement.executeQuery());
         statement.close();
+        connection.close();
         return cooperators;
     }
 
     public Cooperator findByEmail(String email) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("SELECT * FROM COOPERATORS WHERE EMAIL=?");
         statement.setObject(1, email);
         ResultSet resultSet = statement.executeQuery();
         Cooperator cooperator = createCoopFromResultSet(resultSet);
+        resultSet.close();
         statement.close();
+        connection.close();
         return cooperator;
     }
 
     public Cooperator findById(int id) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("SELECT * FROM COOPERATORS WHERE ID=?");
         statement.setObject(1, id);
         ResultSet resultSet = statement.executeQuery();
         Cooperator cooperator = createCoopFromResultSet(resultSet);
+        resultSet.close();
         statement.close();
+        connection.close();
         return cooperator;
     }
 
 
     public void deleteCoop(int id) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("DELETE FROM COOPERATORS WHERE ID=?");
         statement.setObject(1, id);
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 
     public void createCoop(String name, String email, int depId, int salary, Date date) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement(
                         "INSERT INTO COOPERATORS(NAME, EMAIL, DEPARTMENT_ID, SALARY, DATE) VALUES(?,?,?,?,?)");
         statement.setObject(1, name);
@@ -67,10 +79,12 @@ public class CooperatorsService {
         statement.setObject(5, new java.sql.Date(date.getTime()));
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 
     public void updateCooperator(Cooperator cooperator) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement(
                         "UPDATE COOPERATORS SET NAME=?, EMAIL=?, SALARY=?, DATE=? WHERE ID=?");
         statement.setObject(1, cooperator.getName());
@@ -80,6 +94,7 @@ public class CooperatorsService {
         statement.setObject(5, cooperator.getId());
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 
     private Cooperator createCoopFromResultSet(ResultSet resultSet) throws SQLException {
--- src/main/java/database/service/DepartmentService.java
+++ src/main/java/database/service/DepartmentService.java
@@ -5,6 +5,7 @@ import database.entity.Cooperator;
 import database.entity.Department;
 import database.service.utils.ListCreator;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -16,76 +17,96 @@ public class DepartmentService {
     private CooperatorsService cooperatorsService;
 
     public DepartmentService() {
-        this.jdbcPostgreSQL = JDBCPostgreSQL.getInstance();
-        cooperatorsService = new CooperatorsService();
+        this.jdbcPostgreSQL = new JDBCPostgreSQL();
+        this.cooperatorsService = new CooperatorsService();
     }
 
     public List<Department> findAll() throws SQLException {
-        return ListCreator.createDepList(jdbcPostgreSQL.getResultSet(
-                "SELECT * FROM DEPARTMENTS"));
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection.prepareStatement("SELECT * FROM DEPARTMENTS");
+        List<Department> departments = ListCreator.createDepList(statement.executeQuery());
+        statement.close();
+        connection.close();
+        return departments;
     }
 
     public Department findByName(String name) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("SELECT * FROM DEPARTMENTS WHERE NAME=?");
         statement.setObject(1, name);
         ResultSet resultSet = statement.executeQuery();
-        if (resultSet.next()) {
-            int id = resultSet.getInt("ID");
+        try {
+            if (resultSet.next()) {
+                int id = resultSet.getInt("ID");
+                return new Department(id, name);
+            } else {
+                connection.close();
+                return null;
+            }
+        }finally {
+            resultSet.close();
             statement.close();
-            return new Department(id, name);
-        } else {
-            statement.close();
-            return null;
+            connection.close();
         }
     }
 
     public Department findDepById(int id) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("SELECT * FROM DEPARTMENTS WHERE ID=?");
         statement.setObject(1, id);
         ResultSet resultSet = statement.executeQuery();
-        if (resultSet.next()) {
-            String name = resultSet.getString("NAME");
+        try {
+            if (resultSet.next()) {
+                String name = resultSet.getString("NAME");
+                return new Department(id, name);
+            } else {
+                return null;
+            }
+        }finally {
+            resultSet.close();
             statement.close();
-            return new Department(id, name);
-        } else {
-            statement.close();
-            return null;
+            connection.close();
         }
     }
 
     public void createDepartment(String name) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("INSERT INTO DEPARTMENTS(NAME) VALUES(?)");
         statement.setObject(1, name);
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 
     public void deleteDepartment(int id) throws SQLException {
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
         List<Cooperator> cooperators = cooperatorsService.findAllCoopFromDep(id);
         PreparedStatement statement;
         if (cooperators != null) {
-            statement = jdbcPostgreSQL.getDatabaseConnection()
+            statement = connection
                     .prepareStatement("DELETE FROM COOPERATORS WHERE DEPARTMENT_ID=?");
             statement.setObject(1, id);
             statement.executeUpdate();
-            statement.close();
         }
-        statement = jdbcPostgreSQL.getDatabaseConnection()
+        statement = connection
                 .prepareStatement("DELETE FROM DEPARTMENTS WHERE ID=?");
         statement.setObject(1, id);
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 
     public void updateDepartment(Department department) throws SQLException {
-        PreparedStatement statement = jdbcPostgreSQL.getDatabaseConnection()
+        Connection connection = jdbcPostgreSQL.getDatabaseConnection();
+        PreparedStatement statement = connection
                 .prepareStatement("UPDATE  DEPARTMENTS SET NAME=? WHERE ID=?");
         statement.setObject(1, department.getName());
         statement.setObject(2, department.getId());
         statement.executeUpdate();
         statement.close();
+        connection.close();
     }
 }
